830e7594323902145dea6c8ef7f472bcdd1a8adb
Kirill Diduk <kirill.diduk@mail.ru>
2018-03-25 19:33:28 +0300

    Create Maven project.

---
+++ src/main/java/com/kdiduk/yakfighter/App.java
@@ -0,0 +1,13 @@
+package com.kdiduk.yakfighter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
