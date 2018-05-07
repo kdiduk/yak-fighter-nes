aa3a8b1bb3009a7d13b0cf4581772199106932ac
Kirill Diduk <kirill.diduk@mail.ru>
2018-05-07 22:40:36 +0300

    Create test Python app

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from yak_fighter.game import Game
+
+
+def main():
+    game = Game()
+    game.run()
+
+
+if __name__ == '__main__':
+    main()
+
+# eof
---
+++ yak_fighter/game.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+class Game:
+    def run(self):
+        print('Yak the Fighter!')
+
+# eof
