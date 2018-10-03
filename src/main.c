7bef26b7e0b74d9fc7221cbc36ea62ecb71f0d2e
Kirill Diduk <kirill.diduk@mail.ru>
2018-10-03 22:44:56 +0200

    Use example from Shiru (bouncing balls)

--- src/main.c
+++ src/main.c
@@ -1,73 +1,91 @@
-/*	simple Hello World, for cc65, for NES
- *  writing to the screen with rendering disabled
- *	using neslib
- *	Doug Fraker 2018
- */	
- 
- 
- 
+//this example shows how to set up a palette and use 8x8 HW sprites
+//also shows how fast (or slow) C code is
+
 #include "neslib.h"
 
-#define BLACK 0x0f
-#define DK_GY 0x00
-#define LT_GY 0x10
-#define WHITE 0x30
-// there's some oddities in the palette code, black must be 0x0f, white must be 0x30
- 
- 
- 
-#pragma bss-name(push, "ZEROPAGE")
 
-// GLOBAL VARIABLES
-// all variables should be global for speed
-// zeropage global is even faster
+//general purpose vars
 
-unsigned char i;
+static unsigned char i,j;
+static unsigned char spr;
+
+//total number of balls on the screen
+//since there are 64 HW sprites, it is absolute max
+
+#define BALLS_MAX	64
+
+//balls parameters
+
+static unsigned char ball_x[BALLS_MAX];
+static unsigned char ball_y[BALLS_MAX];
+static unsigned char ball_dx[BALLS_MAX];
+static unsigned char ball_dy[BALLS_MAX];
 
 
+//palette for balls, there are four sets for different ball colors
 
-const unsigned char text[]="Hello World!"; // zero terminated c string
-
-const unsigned char palette[]={
-	BLACK, DK_GY, LT_GY, WHITE,
-	0,0,0,0,
-	0,0,0,0,
-	0,0,0,0
+const unsigned char palSprites[16]={
+	0x0f,0x17,0x27,0x37,
+	0x0f,0x11,0x21,0x31,
+	0x0f,0x15,0x25,0x35,
+	0x0f,0x19,0x29,0x39
 };
 
 
 
-	
+void main(void)
+{
+	pal_spr(palSprites);//set palette for sprites
 
-void main (void) {
-	
-	ppu_off(); // screen off
+	ppu_on_all();//enable rendering
 
-	pal_bg(palette); //	load the BG palette
-		
-	// set a starting point on the screen
-	// vram_adr(NTADR_A(x,y));
-	vram_adr(NTADR_A(10,14)); // screen is 32 x 30 tiles
+	//initialize balls parameters
 
-	i = 0;
-	while(text[i]){
-		vram_put(text[i]); // this pushes 1 char to the screen
-		++i;
-	}	
+	for(i=0;i<BALLS_MAX;++i)
+	{
+		//starting coordinates
+
+		ball_x[i]=rand8();
+		ball_y[i]=rand8();
+
+		//direction bits
+
+		j=rand8();
+
+		//horizontal speed -3..-3, excluding 0
+		spr=1+(rand8()%3);
+		ball_dx[i]=j&1?-spr:spr;
+
+		//vertical speed
+
+		spr=1+(rand8()%3);
+		ball_dy[i]=j&2?-spr:spr;
+	}
+
 	
-	// vram_adr and vram_put only work with screen off
-	// NOTE, you could replace everything between i = 0; and here with...
-	// vram_write(text,sizeof(text));
-	// does the same thing
-	
-	ppu_on_all(); //	turn on screen
-	
-	
-	while (1){
-		// infinite loop
-		// game code can go here later.
-		
+	//now the main loop
+
+	while(1)
+	{
+		ppu_wait_frame();//wait for next TV frame
+
+		spr=0;
+
+		for(i=0;i<BALLS_MAX;++i)
+		{
+			//set a sprite for current ball
+
+			spr=oam_spr(ball_x[i],ball_y[i],0x40,i&3,spr);//0x40 is tile number, i&3 is palette
+
+			//move the ball
+
+			ball_x[i]+=ball_dx[i];
+			ball_y[i]+=ball_dy[i];
+
+			//bounce the ball off the edges
+
+			if(ball_x[i]>=(256-8)) ball_dx[i]=-ball_dx[i];
+			if(ball_y[i]>=(240-8)) ball_dy[i]=-ball_dy[i];
+		}
 	}
 }
-	
-	
