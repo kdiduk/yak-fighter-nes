0f3a062876d85753c919068bc43b40d34362e7b0
Kirill Diduk <kirill.diduk@mail.ru>
2018-10-07 15:32:42 +0200

    Project structure re-organized
    Rewrite Makefile and re-organize project file. Split source files from
    include files and tileset.chr file. Rewrite Makefile so that object
    files and other intermediate building files are put in separate obj
    directory. Target nes file is put under bin subdirectory.

---
+++ Makefile
@@ -0,0 +1,96 @@
+# #############################################################################
+#
+# MIT License
+# 
+# Copyright (c) 2018 Kirill Diduk
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# #############################################################################
+
+
+# "Yak Fighter", a simple shoot-em-up game for Nintendo Entertainment System
+#
+# This is the single makefile for GNU Make tool for cross-compiling the
+# project using cc65 toolchain. It contains rules to compile/assemble C and
+# Assembly source files. C files are compiled into Assembly files first.
+#
+# File created on 07 October, 2018
+# Barcelona, Spain
+
+SHELL = /bin/sh
+
+.SUFFIXES:
+.SUFFIXES: .c .s .o
+
+VPATH = ./include:./src
+
+BINDIR = bin
+OBJDIR = obj
+INCDIR = include
+DATDIR = data
+
+# Keep intermediate assembly files, generated when compiling C files
+.PRECIOUS: $(OBJDIR)/%.s
+
+ROMCFG = cartridge.cfg
+TARGET = yak-fighter.nes
+LABELS = labels.txt
+
+OBJECTS = $(addprefix $(OBJDIR)/, main.o startup.o)
+LIBS = nes.lib
+
+AS = ca65
+CC = cc65
+LD = ld65
+
+AFLAGS = -g -I $(INCDIR) --bin-include-dir $(DATDIR)
+CFLAGS = -Oirs -I $(INCDIR) --add-source
+
+# Rules
+
+all: $(BINDIR)/$(TARGET)
+
+# Create output directories before compiling any source files
+$(OBJECTS): | $(OBJDIR)
+$(OBJDIR):
+	mkdir -p $(OBJDIR)
+	mkdir -p $(BINDIR)
+
+$(BINDIR)/$(TARGET): $(OBJECTS)
+	$(LD) -C $(ROMCFG) $(OBJECTS) $(LIBS) -Ln $(OBJDIR)/$(LABELS) -o $@
+
+# Compiling C files into Assembly files
+$(OBJDIR)/%.s: %.c
+	$(CC) $(CFLAGS) $< -o $@
+
+# Assembly files created from C files by the previous rule (above)
+$(OBJDIR)/%.o: $(OBJDIR)/%.s
+	$(AS) -g $< -o $@
+
+# Source assembly files of the project (not created from C files)
+$(OBJDIR)/%.o: %.s
+	$(AS) $(AFLAGS) $< -o $@
+
+
+.PHONY: clean
+clean:
+	rm -rf obj
+
+# end of file
--- src/main.c
+++ src/main.c
@@ -1,91 +1,145 @@
-//this example shows how to set up a palette and use 8x8 HW sprites
-//also shows how fast (or slow) C code is
+/* ****************************************************************************
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2018 Kirill Diduk
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * ************************************************************************* */
 
+#include <stdint.h>
 #include "neslib.h"
 
 
 //general purpose vars
+static uint8_t i,j;
+static uint8_t spr;
+static uint16_t addr;
 
-static unsigned char i,j;
-static unsigned char spr;
+// total number of balls on the screen
+// since there are 64 HW sprites, it is absolute max
+#define BALLS_MAX   (64)
 
-//total number of balls on the screen
-//since there are 64 HW sprites, it is absolute max
+// balls parameters
+static uint8_t ball_x[BALLS_MAX];
+static uint8_t ball_y[BALLS_MAX];
+static uint8_t ball_dx[BALLS_MAX];
+static uint8_t ball_dy[BALLS_MAX];
 
-#define BALLS_MAX	64
+const uint8_t bg_palette[] = {
+    0x02, 0x12, 0x22, 0x32,
+    0x22, 0x36, 0x17, 0x0F,
+    0x22, 0x30, 0x21, 0x0F,
+    0x22, 0x27, 0x17, 0x0F
+};
 
-//balls parameters
-
-static unsigned char ball_x[BALLS_MAX];
-static unsigned char ball_y[BALLS_MAX];
-static unsigned char ball_dx[BALLS_MAX];
-static unsigned char ball_dy[BALLS_MAX];
-
-
-//palette for balls, there are four sets for different ball colors
-
-const unsigned char palSprites[16]={
-	0x0f,0x17,0x27,0x37,
-	0x0f,0x11,0x21,0x31,
-	0x0f,0x15,0x25,0x35,
-	0x0f,0x19,0x29,0x39
+// palette for balls, there are four sets for different ball colors
+const uint8_t sp_palette[16]={
+    0x0f,0x17,0x27,0x37,
+    0x0f,0x11,0x21,0x31,
+    0x0f,0x15,0x25,0x35,
+    0x0f,0x19,0x29,0x39
 };
 
 
+const uint8_t nametable_row[64] = {
+    0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,
+    0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,0x01,
+
+    0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,
+    0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11,0x10,0x11
+};
+
+const uint8_t nametable_attr[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
 
 void main(void)
 {
-	pal_spr(palSprites);//set palette for sprites
+    ppu_off();
+    pal_bg(bg_palette);
+    pal_spr(sp_palette);//set palette for sprites
 
-	ppu_on_all();//enable rendering
+    bank_bg(1);
+    addr = NAMETABLE_A;
+    for (i = 0; i < 15; ++i)
+    {
+        vram_adr(addr);
+        vram_write(nametable_row, sizeof(nametable_row));
+        addr += sizeof(nametable_row);
+    }
+    
+    vram_adr(addr);
+    vram_write(nametable_attr, 8);
+    
+    ppu_on_all();//enable rendering
 
-	//initialize balls parameters
+    //initialize balls parameters
 
-	for(i=0;i<BALLS_MAX;++i)
-	{
-		//starting coordinates
+    for(i=0; i<BALLS_MAX; ++i)
+    {
+        // starting coordinates
+        ball_x[i] = rand8();
+        ball_y[i] = rand8();
 
-		ball_x[i]=rand8();
-		ball_y[i]=rand8();
+        //direction bits
+        j = rand8();
 
-		//direction bits
+        //horizontal speed -3..-3, excluding 0
+        spr = 1 + (rand8() % 3);
+        ball_dx[i] = (j & 1) ? (-spr): (spr);
 
-		j=rand8();
+        //vertical speed
+        spr = 1 + (rand8() % 3);
+        ball_dy[i] = (j & 2) ? (-spr) : (spr);
+    }
 
-		//horizontal speed -3..-3, excluding 0
-		spr=1+(rand8()%3);
-		ball_dx[i]=j&1?-spr:spr;
+    // now the main loop
+    while (1)
+    {
+        ppu_wait_frame(); //wait for next TV frame
 
-		//vertical speed
+        spr=0;
 
-		spr=1+(rand8()%3);
-		ball_dy[i]=j&2?-spr:spr;
-	}
+        for (i=0; i<BALLS_MAX; ++i)
+        {
+            //set a sprite for current ball
+            //0x40 is tile number, i&3 is palette
+            spr = oam_spr(ball_x[i], ball_y[i], 0x75, i&3, spr);
 
-	
-	//now the main loop
+            //move the ball
+            ball_x[i] += ball_dx[i];
+            ball_y[i] += ball_dy[i];
 
-	while(1)
-	{
-		ppu_wait_frame();//wait for next TV frame
+            //bounce the ball off the edges
+            if (ball_x[i] >= (256-8))
+            {
+                ball_dx[i] =- ball_dx[i];
+            }
 
-		spr=0;
-
-		for(i=0;i<BALLS_MAX;++i)
-		{
-			//set a sprite for current ball
-
-			spr=oam_spr(ball_x[i],ball_y[i],0x40,i&3,spr);//0x40 is tile number, i&3 is palette
-
-			//move the ball
-
-			ball_x[i]+=ball_dx[i];
-			ball_y[i]+=ball_dy[i];
-
-			//bounce the ball off the edges
-
-			if(ball_x[i]>=(256-8)) ball_dx[i]=-ball_dx[i];
-			if(ball_y[i]>=(240-8)) ball_dy[i]=-ball_dy[i];
-		}
-	}
+            if (ball_y[i] >= (240-8))
+            {
+                ball_dy[i] =- ball_dy[i];
+            }
+        }
+    }
 }
+
+/* eof */
